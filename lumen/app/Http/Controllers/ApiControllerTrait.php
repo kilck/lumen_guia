ba9bd1e79b5ccca43ce15ea9f0aa87fe2844a5bf
kilck <fullcabana@gmail.com>
2018-03-15 17:18:04 -0300

    Atualizando registros

--- lumen/app/Http/Controllers/ApiControllerTrait.php
+++ lumen/app/Http/Controllers/ApiControllerTrait.php
@@ -70,6 +70,14 @@ trait ApiControllerTrait
         return response()->json($result);
     }
 
+    public function update(Request $request, $id)
+    {
+        $result = $this->model->findOrFail($id);
+        $result->update($request->all());
+        return response()->json($result);
+    }
+
+
     protected  function relationships()
     {
         if (isset($this->relationships)){
--- lumen/routes/web.php
+++ lumen/routes/web.php
@@ -19,4 +19,5 @@ $router->group(['prefix' => 'api/v1', 'namespace' => 'Api\V1'], function () use 
     $router->get('restaurants','RestaurantsController@index');
     $router->get('restaurants/{id}','RestaurantsController@show');
     $router->post('restaurants','RestaurantsController@store');
+    $router->put('restaurants/{id}','RestaurantsController@update');
 });
