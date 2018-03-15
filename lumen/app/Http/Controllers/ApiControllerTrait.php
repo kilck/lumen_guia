b5eedabae7bc5166091de9a79f9c5c1b821a8c55
kilck <fullcabana@gmail.com>
2018-03-15 16:42:52 -0300

    Incrementando a listagem de dados

--- lumen/app/Http/Controllers/ApiControllerTrait.php
+++ lumen/app/Http/Controllers/ApiControllerTrait.php
@@ -1,11 +1,66 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
 trait ApiControllerTrait
 {
-    public function index()
+    public function index(Request $request)
     {
-        $reseults = $this->model->paginate();
+        /**
+         * Filtra os dados com base na query string
+         *
+         * Exemplos:
+         *
+         * ?limit=15
+         * ?order=created_at,desc
+         * ?where[id]=2
+         * ?like[name]=erik
+         *
+         * @param Request $request
+         * @return \Illuminate\Http\JsonResponse
+         */
+        $data = $request->all();
+        // $limit = empty($data['limit']) ? $data['limit'] : 20;
+        $limit = $data['limit'] ?? 20;
+
+        $order = $data['limit'] ?? null;
+        if ($order !== null) {
+            $order = explode(',', $order);
+        }
+        $order[0] = $order[0] ?? 'id';
+        $order[1] = $order[1] ?? 'asc';
+
+        $where = $data['where'] ?? [];
+        $like = null;
+        if (!is_array($data['like'])){
+            $like = $data['like']
+
+            $key = key(reset($like));
+            $like[0] = $key;
+            $like[1] = '%'.$like[$key].'%';
+        }
+
+        $reseults = $this->model
+            ->orderBy($order[0], $order[1])
+            ->where(function ($query) use ($like){
+                if ($like){
+                    return $query->where($like[0], 'like', $like[1]);
+                }
+                return $query;
+            })
+            ->where($where);
+            ->with($this->relationships())
+            ->paginate($limit);
         return response()->json($reseults);
+    }
+
+    protected  function relationships()
+    {
+        if (isset($this->relationships)){
+            return $this->relationships;
+        }
+        return[];
     }
 }
